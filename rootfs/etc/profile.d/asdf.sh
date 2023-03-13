9e1aa6620efec5813b40e4895f1291e5c7579376
luminousreseal <124809434+luminousreseal@users.noreply.github.com>
2023-03-13 20:45:28 +0000

    fix: source correct asdf.sh

--- rootfs/etc/profile.d/asdf.sh
+++ rootfs/etc/profile.d/asdf.sh
@@ -2,7 +2,7 @@ export ASDF_DIR="${ASDF_DIR:-"/opt/asdf-vm"}"
 
 if [ -n "$BASH_VERSION" ]; then
 	# shellcheck disable=SC1091
-	. "${ASDF_DIR}/opt/asdf/asdf.sh"
+	. "${ASDF_DIR}/asdf.sh"
 	# shellcheck disable=SC1091
 	. "${ASDF_DIR}/completions/asdf.bash"
 fi
